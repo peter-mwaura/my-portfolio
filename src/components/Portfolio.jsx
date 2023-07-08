ebee353761cfd99ab796b554a38514e6bb4c4894
peter-mwaura <peterkaranjagitz@gmail.com>
2023-07-08 05:37:23 +0300

    refactor: add ongoing sportstats project to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import todoApp from "../assets/portfolio/todoApp.png";
 import myPortfolio from "../assets/portfolio/my-portfolio.png";
+import sportStats from "../assets/portfolio/sportstats.png";
 import technicalDocumentation from "../assets/portfolio/technicalDocumentation.png";
 import gridTestimonials from "../assets/portfolio/gridTestimonials.png";
 import catGallery from "../assets/portfolio/catGallery.png";
@@ -21,19 +22,25 @@ const Portfolio = () => {
     },
     {
       id: 3,
+      src: sportStats,
+      demo: "https://sportstats.netlify.app/",
+      github: "https://github.com/peter-mwaura/sports-stats",
+    },
+    {
+      id: 4,
       src: technicalDocumentation,
       demo: "https://peter-mwaura.github.io/javascript-technical-documentation/",
       github:
         "https://github.com/peter-mwaura/javascript-technical-documentation",
     },
     {
-      id: 4,
+      id: 5,
       src: gridTestimonials,
       demo: "https://peter-mwaura.github.io/grid-testimonials-page/",
       github: "https://github.com/peter-mwaura/grid-testimonials-page",
     },
     {
-      id: 5,
+      id: 6,
       src: catGallery,
       demo: "https://peter-mwaura.github.io/flexbox-photo-gallery/",
       github: "https://github.com/peter-mwaura/flexbox-photo-gallery",
